a0df6ddab2a6bc0beb7f5f9443ea950b78db6476
PZZ97 <13573227027@163.com>
2022-11-16 18:35:59 -0500

    No ideas

--- lab/07_pio_sequencer/code/REPL/main.c
+++ lab/07_pio_sequencer/code/REPL/main.c
@@ -6,6 +6,7 @@
 #include "register.h"
 #include "ws2812.pio.h" // pio0
 #include "hardware/dma.h"
+#include "hardware/structs/bus_ctrl.h"
 #define QTPY_BOOT_PIN 21
 
 #define E_READ 'r'
@@ -15,7 +16,11 @@
 #define E_SETPIN 'p'
 #define E_WRITEIO '>'
 // default read pin
-int g_Pin;
+int g_Pin=21;
+
+PIO g_pio;
+uint g_sm;
+uint g_dma_chan;
 
 void funcRead(){
     ADDRESS address;
@@ -44,7 +49,10 @@ void funcRGB(){
 }
 void funcSetPin(){
     scanf("%d",&g_Pin);
-    // gpio_set_dir(g_Pin, GPIO_IN);
+    pio_selfinit(g_pio,g_sm,g_Pin,1.f);
+    gpio_set_dir(g_Pin, GPIO_IN);
+    gpio_init(g_Pin);
+    neopixel_set_rgb(0x000f0000);   //green
 }
 // void funcGpioRead(){
 //     int pin=0;  // must use type int (not uint_8) and %d in scanf
@@ -61,8 +69,35 @@ void funcGpioReadDefault(){
     // printf("o%d",gpio_get(g_Pin));
     uint32_t read;
 
-    gpio_pio_read(&read,g_Pin);
+    uint16_t capture_prog_instr = pio_encode_in(pio_pins, 1);   
+    struct pio_program capture_prog = {
+            .instructions = &capture_prog_instr,
+            .length = 1,
+            .origin = -1
+    };
+    uint offset = pio_add_program(g_sm, &capture_prog);  
+    pio_sm_config c = pio_get_default_sm_config();
+    sm_config_set_in_pins(&c, g_Pin);
+    
+    sm_config_set_in_shift(&c, true, true, 1);
+    sm_config_set_fifo_join(&c, PIO_FIFO_JOIN_RX);
+    pio_sm_init(g_pio, g_sm, offset, &c);
+
+    // gpio_pio_read(g_pio,g_sm,g_dma_chan,&read,g_Pin);
+    pio_sm_set_enabled(g_pio, g_sm, false);
+    pio_sm_clear_fifos(g_pio, g_sm);
+    pio_sm_restart(g_pio, g_sm);
+
+
+    pio_sm_exec(g_pio, g_sm,  pio_encode_in(pio_pins, 1)); 
+
+    // pio_sm_exec(pio0, 0,  pio_encode_push(1, 0)); 
+    pio_sm_set_enabled(g_pio, g_sm, true);
+    read= pio_sm_get_blocking (g_pio,g_sm);
+
+    // dma_channel_wait_for_finish_blocking(g_dma_chan);
     printf("o%d",read);
+    printf(",o");
 }
 void funcPIOWrite(){
     char value= 0xff;
@@ -75,23 +110,24 @@ void funcPIOWrite(){
         neopixel_set_rgb(0x0000ff00);  
     // printf("%c",value);
 }
-void PIO_init(PIO pio, uint sm, uint pin_base,  float div) {
-    // uint16_t capture_prog_instr = pio_encode_in(pio_pins, 1);   /*  => $ IN pio_pins pin_count
-    //                                                                         (pio_pins=0, source =PINS)
-    //                                                                         pin_count=2, shift 2bits to ISR 
-    //                                                                         datasheet 3.2.3.2 ISR  */
-    // struct pio_program capture_prog = {
-    //         .instructions = &capture_prog_instr,
-    //         .length = 1,
-    //         .origin = -1
-    // };
-    // uint offset = pio_add_program(pio, &capture_prog);                  /*  Create the pio program */
+
+void pio_selfinit(PIO pio, uint sm, uint pin_base,  float div) {
+    uint16_t capture_prog_instr = pio_encode_in(pio_pins, 1);   /*  => $ IN pio_pins pin_count
+                                                                            (pio_pins=0, source =PINS)
+                                                                            pin_count=2, shift 2bits to ISR 
+                                                                            datasheet 3.2.3.2 ISR  */
+    struct pio_program capture_prog = {
+            .instructions = &capture_prog_instr,
+            .length = 1,
+            .origin = -1
+    };
+    uint offset = pio_add_program(pio, &capture_prog);                  /*  Create the pio program */
 
     // Configure state machine to loop over this `in` instruction forever,
     // with autopush enabled.
     pio_sm_config c = pio_get_default_sm_config();
     sm_config_set_in_pins(&c, pin_base);
-    // sm_config_set_wrap(&c, offset, offset);
+    sm_config_set_wrap(&c, offset, offset);
     sm_config_set_clkdiv(&c, div);
     // Note that we may push at a < 32 bit threshold if pin_count does not
     // divide 32. We are using shift-to-right, so the sample data ends up
@@ -103,15 +139,8 @@ void PIO_init(PIO pio, uint sm, uint pin_base,  float div) {
     // pio_sm_init(pio, sm, offset, &c);
 }
 
-// void gpio_pio_read(PIO pio, uint sm, uint dma_chan, uint32_t *read_buf,uint32_t pin) {
-void gpio_pio_read(uint32_t *read_buf,uint32_t pin) {
-    gpio_init(pin);
-    gpio_set_dir(pin, GPIO_IN);
-    PIO pio = pio0;
-    uint sm = 0;
-    uint dma_chan = 0;
-    PIO_init(pio,sm,pin,1.f);
-
+void gpio_pio_read(PIO pio, uint sm,uint dma_chan,uint32_t *read_buf,uint32_t pin) {
+    pio_sm_set_enabled(pio, sm, false);
     pio_sm_clear_fifos(pio, sm);
     pio_sm_restart(pio, sm);
 
@@ -126,14 +155,18 @@ void gpio_pio_read(uint32_t *read_buf,uint32_t pin) {
         1, // Number of transfers
         true                // Start immediately
     );
-    pio_sm_exec(pio, sm,  pio_encode_in(pio_pins, 1)); /* stall until the first up edge */
+    // pio_sm_exec(pio, sm,  pio_encode_in(pio_pins, 1)); /* stall until the first up edge */
+    pio_sm_set_enabled(pio, sm, true);
+    neopixel_set_rgb(0x0000ff00);   //blue
 }
 int main() {
     stdio_init_all();
     while(stdio_usb_connected()!=true);
+    g_pio = pio0;
+    g_sm = 0;
+    g_dma_chan = 0;
+    bus_ctrl_hw->priority = BUSCTRL_BUS_PRIORITY_DMA_W_BITS | BUSCTRL_BUS_PRIORITY_DMA_R_BITS;
 
-    // gpio_init(QTPY_BOOT_PIN);
-    // gpio_set_dir(QTPY_BOOT_PIN, GPIO_IN);
     /////////////////////////////////////////////////////
 
     /////////////////////////////////////////////////////
@@ -158,6 +191,7 @@ int main() {
             funcGpioReadDefault();
         }
         else if(event ==E_SETPIN){
+            neopixel_set_rgb(0x0000000f);   // light blue
             funcSetPin();
         }
         else if(event==E_WRITEIO){
